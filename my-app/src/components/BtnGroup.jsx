ae145eada906cce981a61987ddb7bd7f8a784318
Ivan Ushachov <90464856+VanyaSO@users.noreply.github.com>
2022-09-06 17:34:29 +0300

    updata

--- my-app/src/components/BtnGroup.jsx
+++ my-app/src/components/BtnGroup.jsx
@@ -1,31 +1,50 @@
-import React, { useState } from "react";
+import React from "react";
 import classNames from "classnames";
 
 
-const [activeClass, setActiveClass] = useState("");
+// const [activeClass, setActiveClass] = useState("");
 
 class BtnGroup extends React.Component{
-
-    classLeftBtn = classNames([
-        "btn btn-danger",
-        "left" === activeClass ? 'active' : ''
-    ])
-
-    classRightBtn = classNames([
-        "btn btn-danger",
-        "right" === activeClass ? 'active' : ''
-    ])
+    constructor(props) {
+        super(props);
+        this.state = {
+            activeClass: ""
+        }
+    }
 
     addClass = (event) => {
-        if(event.target.value === "leftBtn") return setActiveClass('left');
-        return setActiveClass("right");
+        if(event.target.value === "leftBtn"){
+            return this.setState((state) => {
+                return{
+                    ...state,
+                    activeClass: 'left'
+                }
+            });
+        }
+        return this.setState((state) => {
+            return{
+                ...state,
+                activeClass: 'right'
+            }
+        });
     }
 
     render() {
+
+        const classLeftBtn = classNames([
+            "btn btn-danger",
+            "left" === this.state.activeClass ? 'active' : ''
+        ])
+
+        const classRightBtn = classNames([
+            "btn btn-danger",
+            "right" === this.state.activeClass ? 'active' : ''
+        ])
+
         return (
             <div className="d-flex justify-content-center mt-5" role="group">
-                <button type="button" onClick={this.addClass} value="leftBtn" className={this.classLeftBtn}>1</button>
-                <button type="button" onClick={this.addClass} value="rightBtn" className={this.classRightBtn}>2</button>
+                <button type="button" onClick={this.addClass} value="leftBtn" className={classLeftBtn}>1</button>
+                <button type="button" onClick={this.addClass} value="rightBtn" className={classRightBtn}>2</button>
             </div>
         )
     }
